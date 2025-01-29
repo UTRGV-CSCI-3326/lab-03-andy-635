5d08d580fdd998aedbf1a250fbd48d7ef98a3b5d
andy-635 <161386628+andy-635@users.noreply.github.com>
2025-01-29 20:15:11 +0000

    Commit 3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
-
+public class BankAccount{
+    public static void main (String[] args){
+        float bank = 2175.37f;
+        bank-= 302.50;
+        bank += 50.03;
+        bank/=2;
+        bank+=20;
+        bank--;
+        bank*=2;
+        bank++;
+        System.out.println("New bank account balance is: " + bank);
+        
+    }
+}
